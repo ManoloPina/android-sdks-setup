b2446c37405ff473d30a983461d77b2e240f0a44
Manolo Pina <contato@manolopina.com.br>
2018-01-17 11:05:26 -0200

    Implementação facebook analytics

--- app/src/main/java/com/example/manolopina/myapplication/MainActivity.java
+++ app/src/main/java/com/example/manolopina/myapplication/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.manolopina.myapplication;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    AppEventsLogger logger = AppEventsLogger.newLogger(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logSentFriendRequestEvent () {
+        logger.logEvent("sentFriendRequest");
     }
 }
